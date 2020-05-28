f775d7d585155bffdb9f66a76c3c24bb55ce9cf9
Brian J Brennan <brianloveswords@gmail.com>
2020-05-28 17:52:51 -0400

    Add encoded credentials????

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -l
 
+if [ -n "${GOOGLE_CREDENTIALS_ENCODED}" ]; then
+    mkdir -p "${HOME}"/.config/gcloud
+    export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/.config/gcloud/credentials.json
+    echo "${GOOGLE_CREDENTIALS_ENCODED}" | python -m base64 -d | gzip -d >"${HOME}"/.config/gcloud/credentials.json
+else
+    error 'must set GOOGLE_CREDENTIALS_ENCODED'
+fi
+
 echo Hello world my name is "${INPUT_MY_NAME}"
 node -e 'console.log("hi hello")'
+jq <"${HOME}"/.config/gcloud/credentials.json
 sh -c "ls -lah"
